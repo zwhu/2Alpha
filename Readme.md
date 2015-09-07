6f34dccb20826f8f421a4f5c48e0021e095895d8
胡正伟 <331946763@qq.com>
2015-09-07 17:01:04 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# 介绍
+
+不会用`ps`的苦逼程序员将老板给的logo.jpg图片中的白色部分转成alpha通道的png图片
+
+
+### 使用方法
+
+
+依赖 node-canvas 库，此库安装非常麻烦
+[osx的安装方法](https://github.com/Automattic/node-canvas/wiki/installation---osx)
+
+```shell
+  babel-node index.js yourawpicpath yousavepicpath
+```
+
+原始图片和需要保存的图片的路径都是相对路径
---
+++ index.js
@@ -0,0 +1,58 @@
+import Canvas  from 'canvas'
+import fs from 'fs'
+
+const Image = Canvas.Image
+
+let img = new Image
+  , start = new Date
+  , rawPath =  process.argv[2]
+  , savePath = process.argv[3]
+
+if(!rawPath)
+  throw new Error('input raw image path')
+
+  if(!savePath)
+    throw new Error('input save image path')
+
+
+img.onerror = function(err){
+  throw err
+}
+
+img.onload = function(){
+
+  let width = img.width
+    , height = img.height
+    , canvas = new Canvas(width, height)
+    , ctx = canvas.getContext('2d')
+
+  ctx.imageSmoothingEnabled = true
+  ctx.drawImage(img, 0, 0, width, height)
+
+  let imageData = ctx.getImageData(0, 0, width, height)
+    , data = imageData.data
+    , newData = [], len = data.length/4
+
+  for(let i = 0; i < data.length; i+=4) {
+    let r = data[i]
+      , g = data[i+1]
+      , b = data[i+2]
+
+    if([r,g,b].every(v => v < 256 && v > 240)) data[i+3] = 0
+  }
+  //
+  ctx.putImageData(imageData, 0, 0)
+
+  let out = fs.createWriteStream(`${__dirname}/${savePath}`)
+    , stream = canvas.pngStream()
+
+  stream.on('data', function (chunk) {
+    out.write(chunk)
+  })
+
+  stream.on('end', function () {
+    console.log(`保存到 ${__dirname}/${savePath}`)
+  })
+}
+
+img.src = `${__dirname}/${rawPath}`
